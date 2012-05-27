147aa944b1152241af89fd37b0ae402827b9109f
Hendrik Mans <hendrik@mans.de>
2012-05-27 22:24:55 +0200

    Flesh out spec

--- spec/permissions_spec.rb
+++ spec/permissions_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 module Allowance
   describe Permissions do
-    it "should do something cool" do
+    it "should allow simple permissions to be specified" do
       p = Permissions.new do
         can :moo
       end
 
       p.can?(:moo).should == true
+      p.can?(:quack).should == false
     end
   end
 end
