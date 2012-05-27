0d7f3e12cd839a08f6a1f31a8fcefa3cd5313e54
Hendrik Mans <hendrik@mans.de>
2012-05-27 22:32:30 +0200

    More specs.

--- spec/permissions_spec.rb
+++ spec/permissions_spec.rb
@@ -2,13 +2,26 @@ require 'spec_helper'
 
 module Allowance
   describe Permissions do
+    SomeClass = Class.new
+    SomeOtherClass = Class.new
+
     it "should allow simple permissions to be specified" do
       p = Permissions.new do
         can :moo
       end
 
-      p.can?(:moo).should == true
-      p.can?(:quack).should == false
+      insist p.can?(:moo)
+      refuse p.can?(:quack)
+    end
+
+    it "should allow verbs and objects" do
+      p = Permissions.new do
+        can :update, SomeClass
+      end
+
+      insist p.can?(:update, SomeClass)
+      refuse p.can?(:destroy, SomeClass)
+      refuse p.can?(:update, SomeOtherClass)
     end
   end
 end
