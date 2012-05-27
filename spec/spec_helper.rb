d364cd1fa7a1bcd549b13dd152cb1341d5a47eb2
Hendrik Mans <hendrik@mans.de>
2012-05-27 22:32:18 +0200

    Provide insist() and refuse() helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,11 @@ lib_path = File.expand_path("#{SPEC_DIR}/../lib")
 $LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
 
 require 'allowance'
+
+def insist(what)
+  what.should == true
+end
+
+def refuse(what)
+  what.should == false
+end
